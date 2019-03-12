c79d7e7747314cb8164713909bcc9bbc83840676
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-12 14:48:49 +0100

    Styling with a global css class - importing css possible with webpack

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import './Person.css'
+
 const person = (props) => {
     return (
-        <div>
+        <div
+            className="Person"
+        >
             <p
                 onClick={props.click}
             >
