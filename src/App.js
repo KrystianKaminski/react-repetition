72a1f113182275fd408d481549b723330c9707be
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-21 20:07:32 +0100

    More elegant way of conditional rendering

--- src/App.js
+++ src/App.js
@@ -46,6 +46,30 @@ class App extends React.Component {
       cursor: 'pointer'
     }
 
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHander}
+          > My hobbies: racing
+        </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      )
+    }
+
     return (
       <div>
         <h1>Hi, I'm a React App</h1>
@@ -55,26 +79,7 @@ class App extends React.Component {
           onClick={this.togglePersonsHandler}
         >Switch Person components
         </button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-              />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-                changed={this.nameChangedHander}
-              > My hobbies: racing
-        </Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-              />
-            </div> : null
-        }
+        {persons}
       </div>
     )
   }
