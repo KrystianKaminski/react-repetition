6293af901dcfefd91fcee2510eea09c9c4696239
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-24 11:52:33 +0100

    Pseudo selectors with CSS modules

--- src/App.js
+++ src/App.js
@@ -47,16 +47,9 @@ class App extends React.Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
 
     let persons = null
+    let btnClass = ''
 
     if (this.state.showPersons) {
       persons = (
@@ -73,7 +66,7 @@ class App extends React.Component {
         </div>
       )
 
-      style.backgroundColor = 'red'
+      btnClass = classes.Red
     }
 
     const assignedClasses = []
@@ -90,7 +83,7 @@ class App extends React.Component {
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}
         >Switch Person components
         </button>
