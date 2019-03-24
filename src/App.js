4458aedd089ce4b8c546f6dcaa5026175f0b4445
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-24 10:13:12 +0100

    Media queries in React - possible with Radium package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import Radium from 'radium'
+import Radium, { StyleRoot } from 'radium'
 import Person from './Person/Person'
 
 class App extends React.Component {
@@ -80,7 +80,7 @@ class App extends React.Component {
 
       style.backgroundColor = 'red'
       style[':hover'] = {
-        backgroundColor: 'lightred',
+        backgroundColor: 'salmon',
         color: 'black'
       }
     }
@@ -94,18 +94,20 @@ class App extends React.Component {
     }
 
     return (
-      <div
-        className="App"
-      >
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}
-        >Switch Person components
+      <StyleRoot>
+        <div
+          className="App"
+        >
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}
+          >Switch Person components
         </button>
-        {persons}
-      </div>
+          {persons}
+        </div>
+      </StyleRoot>
     )
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,16 @@ import Radium from 'radium'
 import './Person.css'
 
 const Person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+
     return (
         <div
             className="Person"
+            style={style}
         >
             <p
                 onClick={props.click}
