e1bc151a7b01fb054563e305eb6be67bfb0351de
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-25 09:15:53 +0100

    Controlling the useEffect behavior - it triggers only if persons changed

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,7 +6,13 @@ const Cockpit = props => {
 
     useEffect(() => {
         console.log('[Cockpit.js] useEffect')
-    })
+        // HTTP request ...
+        setTimeout(() => {
+            alert('Saved data to cloud!')
+        }, 1000)
+    }, [props.persons])
+
+    // useEffect()
 
     const assignedClasses = []
     let btnClass = ''
